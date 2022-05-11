a0b6fc4d2da4026b797c3740170271bf05d0c396
Sonja Kuvshinova <kuvshinova.sonja@yandex.ru>
2022-05-11 16:49:07 +0300

    Сделать попап

---
+++ index.js
@@ -0,0 +1,34 @@
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__close-icon');
+
+function togglePopup () {
+ popup.classList.toggle('popup_opened')}
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
+popup.addEventListener('click', togglePopup);
+
+
+// Находим форму в DOM
+let formElement = // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = // Воспользуйтесь инструментом .querySelector()
+let jobInput = // Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
