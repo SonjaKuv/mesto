fc4503fd24376124c292904ce5e0597f6e39bb36
Sonja Kuvshinova <kuvshinova.sonja@yandex.ru>
2022-05-23 14:39:11 +0300

    вывод данных из массива

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,38 @@ const editButton = document.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup_type_edit');
 const addButton = document.querySelector('.profile__add-button');
 const popupAdd = document.querySelector('.popup_type_add');
-const closeButton = popup.querySelector('.popup__close-icon');
+const closeButton = document.querySelector('.popup__close-icon');
+
+
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__input_value_name');
+let jobInput = formElement.querySelector('.form__input_value_job');
+
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
+let itemName = document.querySelector('.grid-item__title');
+let itemImage = document.querySelector('.grid-item__photo');
+
+//открытие окна редактирования профиля, значения инпутов берутся со страницы
+function openPopupEdit() {
+    popupEdit.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+};
+
+// открытие окна добавления контента, инпуты пустые
+function openPopupAdd() {
+    popupAdd.classList.add('popup_opened');
+};
+
+//закрытие любого из попапов
+function closePopup(evt) {
+   evt.currentTarget.classList.remove('popup_opened');
+};
+
+const cardsContainer = document.querySelector('.grid-elements');
+
+
 
 const initialCards = [
   {
@@ -31,31 +62,17 @@ const initialCards = [
   }
 ];
 
-let formElement = popup.querySelector('.form');
-let nameInput = formElement.querySelector('.form__input_value_name');
-let jobInput = formElement.querySelector('.form__input_value_job');
+//заполняем страницу контентом из массива
+initialCards.forEach((element) => {
+        const cardTemplate = document.querySelector('.item-template').content;
+        const cardElement = cardTemplate.querySelector('.grid-item').cloneNode(true);
 
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__description');
-let itemName = document.querySelector('.grid-item__title');
-let itemImage = document.querySelector('.grid-item__photo');
+        cardElement.querySelector('.grid-item__title').textContent = element.name;
+        cardElement.querySelector('.grid-item__photo').src = element.link;
 
-//открытие окна редактирования профиля, значения инпутов берутся со страницы
-function openPopupEdit() {
-    popupEdit.classList.add('popup_opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-}
+        cardsContainer.append(cardElement);
+    });
 
-// открытие окна добавления контента, инпуты пустые
-function openPopupAdd() {
-    popupAdd.classList.add('popup_opened');
-}
-
-//закрытие любого из попапов
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
 
 
 function formSubmitHandler(evt) {
@@ -66,8 +83,8 @@ function formSubmitHandler(evt) {
     closePopup();
 };
 
-editButton.addEventListener('click', openPopup1);
-addButton.addEventListener('click', openPopup2);
+editButton.addEventListener('click', openPopupEdit);
+addButton.addEventListener('click', openPopupAdd);
 closeButton.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
