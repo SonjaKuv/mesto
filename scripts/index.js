705ccd119370c1fe7cb7467e0ba10272acbd9666
Sonja Kuvshinova <kuvshinova.sonja@yandex.ru>
2022-05-23 11:30:52 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,35 @@
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup_type_edit');
+const addButton = document.querySelector('.profile__add-button');
+const popupAdd = document.querySelector('.popup_type_add');
 const closeButton = popup.querySelector('.popup__close-icon');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 let formElement = popup.querySelector('.form');
 let nameInput = formElement.querySelector('.form__input_value_name');
@@ -8,14 +37,22 @@ let jobInput = formElement.querySelector('.form__input_value_job');
 
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
+let itemName = document.querySelector('.grid-item__title');
+let itemImage = document.querySelector('.grid-item__photo');
 
-
-function openPopup() {
-    popup.classList.add('popup_opened');
+//открытие окна редактирования профиля, значения инпутов берутся со страницы
+function openPopupEdit() {
+    popupEdit.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
+// открытие окна добавления контента, инпуты пустые
+function openPopupAdd() {
+    popupAdd.classList.add('popup_opened');
+}
+
+//закрытие любого из попапов
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
@@ -29,7 +66,8 @@ function formSubmitHandler(evt) {
     closePopup();
 };
 
-editButton.addEventListener('click', openPopup);
+editButton.addEventListener('click', openPopup1);
+addButton.addEventListener('click', openPopup2);
 closeButton.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
